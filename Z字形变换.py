b8cf09e7b7b943668d534d2049c866a5755114eb
Chaaaarlieeee <Charlie_ge2023@163.com>
2025-07-03 17:33:22 +0800

    7.3
    区别于传统的创建二维网格，直接使用三组字符串表示转换后的字符，并且使用flag控制赋值逻辑，实现Z形赋值

---
+++ Z字形变换.py
@@ -0,0 +1,13 @@
+#v1
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2: return s
+        res = ["" for _ in range(numRows)]
+        i= 0
+        flag= -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1: flag = -flag
+            i += flag
+        return "".join(res)
+
