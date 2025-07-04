a96c02718fdb36044f73e79cd9c24e9c975f5869
Chaaaarlieeee <Charlie_ge2023@163.com>
2025-07-04 20:14:37 +0800

    Update 字符串转换整数.py
    尝试了评论区中的其他解法

--- 字符串转换整数.py
+++ 字符串转换整数.py
@@ -11,14 +11,14 @@ class Solution:
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31
         
-        if s[index] == '-':
+        if[index] == '-':
             sign = -1
             index += 1
-        elif s[index] == '+':
+        elif[index] == '+':
             index += 1
         
-        while index < len(s) and s[index].isdigit():
-            digit = int(s[index])
+        while index < len() and[index].isdigit():
+            digit = int([index])
             
             if result > INT_MAX // 10 or (result == INT_MAX // 10 and digit > INT_MAX % 10):
                 return INT_MAX if sign == 1 else INT_MIN
@@ -80,11 +80,28 @@ class Solution:
         if not s: return 0                   # 字符串为空则直接返回
         res, i, sign = 0, 1, 1
         int_max, int_min, bndry = 2 ** 31 - 1, -2 ** 31, 2 ** 31 // 10
-        if s[0] == '-': sign = -1            # 保存负号
-        elif s[0] != '+': i = 0              # 若无符号位，则需从 i = 0 开始数字拼接
-        for c in s[i:]:
+        if[0] == '-': sign = -1            # 保存负号
+        elif[0] != '+': i = 0              # 若无符号位，则需从 i = 0 开始数字拼接
+        for c in[i:]:
             if not '0' <= c <= '9' : break     # 遇到非数字的字符则跳出
             if res > bndry or res == bndry and c > '7': return int_max if sign == 1 else int_min # 数字越界处理
             res = 10 * res + ord(c) - ord('0') # 数字拼接
         return sign * res
 
+#v4
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        res, i, sign, length = 0, 0, 1, len()
+        int_max, int_min, bndry = 2 ** 31 - 1, -2 ** 31, 2 ** 31 // 10
+        if not s: return 0         # 空字符串，提前返回
+        while[i] == ' ':
+            i += 1
+            if i == length: return 0 # 字符串全为空格，提前返回
+        if[i] == '-': sign = -1
+        if[i] in '+-': i += 1
+        for j in range(i, length):
+            if not '0' <= s[j] <= '9' : break
+            if res > bndry or res == bndry and[j] > '7':
+                return int_max if sign == 1 else int_min
+            res = 10 * res + ord([j]) - ord('0')
+        return sign * res
