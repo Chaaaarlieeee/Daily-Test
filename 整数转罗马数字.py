ccf096fc5219bd25fb4c1d50c98a77bb2a0b84e3
Charlie Ge <Charlie_ge2023@163.com>
2025-07-06 19:54:15 +0800

    Update 整数转罗马数字.py
    更新了官方题解，降低了运行耗时

--- 整数转罗马数字.py
+++ 整数转罗马数字.py
@@ -17,3 +17,31 @@ class Solution:
                 num=num-head1.val
         return value
 #v2
+class Solution:
+    def intToRoman(self, num: int) -> str:
+            VALUE_SYMBOLS = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+        ]
+            value = str()
+            for val,rom in VALUE_SYMBOLS:
+                while num>=val:
+                    num-=val
+                    value=value+rom
+                if num==0:
+                    break
+            return value
+
+
+            
