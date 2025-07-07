d34b0f8960fb6c7cce54df0ea6c59f72bab50668
Charlie Ge <Charlie_ge2023@163.com>
2025-07-07 16:29:17 +0800

    7.7
    最长公共前缀，纵向搜寻法

---
+++ 最长公共前缀.py
@@ -0,0 +1,30 @@
+#v1
+class Solution:
+    def check_identical_elements_2(self,lst):
+        return len(set(lst)) == 1
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if len(strs)==1:
+            return strs[0]
+        for i in strs:
+            if i=="":
+                return ""
+        table = ["" for _ in range((len(strs)))]
+        shortest_string = min(strs, key=len)
+        n=len(shortest_string)
+        j=0
+        while n>0:
+            if self.check_identical_elements_2(table)!=True:
+                    e=str(table[0])
+                    e=e[:-1]
+                    return e
+
+            for i,s in enumerate(strs):
+                table[i]=table[i]+s[j]
+            j=j+1    
+            n=n-1
+        if self.check_identical_elements_2(table)!=True:
+            e=str(table[0])
+            e=e[:-1]
+            return e
+        else:return table[0]
+        
