ddc9f4be79d03a29a72e4f4121157faa87733902
Charlie Ge <Charlie_ge2023@163.com>
2025-07-17 19:40:20 +0800

    7.17
    盛水最多的容器，遍历法

---
+++ 盛最多水的容器.py
@@ -0,0 +1,19 @@
+#v1
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        table=[]
+        for i,j in enumerate(height):
+            table.append([i,j])
+        left=0
+        right=0
+        area=0
+        current_area=0
+        while left<=len(height):
+            right=left+1
+            while right<len(height):
+                current_area=((right-left)*min(int(table[left][1]),int(table[right][1])))
+                if current_area>area:
+                    area=current_area
+                right+=1
+            left+=1
+        return area
