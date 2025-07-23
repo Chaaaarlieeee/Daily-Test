a806bdd74caa01d71f1fd19f25af6086b657bd10
Charlie Ge <Charlie_ge2023@163.com>
2025-07-23 18:57:33 +0800

    Update 合并K个升序链表.py
    添加了评论区的题解：使用最小堆解法，引入heapq模块，使用heappush为数组赋值时会按照升序排序，按照该规则解题即可

--- 合并K个升序链表.py
+++ 合并K个升序链表.py
@@ -17,3 +17,23 @@ class Solution:
         
         return dummy.next
 
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
+        dummy=ListNode(0)
+        p=dummy
+        head=[]
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(head,(lists[i].val,i))
+                lists[i]=lists[i].next
+        while head:
+            val,idx=heapq.heappop(head)
+            p.next=ListNode(val)
+            p=p.next
+            if lists[idx]:
+                heapq.heappush(head,(lists[idx].val,idx))
+                lists[idx]=lists[idx].next
+        return dummy.next
