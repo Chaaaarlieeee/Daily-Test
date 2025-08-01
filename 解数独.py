2f4e01449aec9ccdcaaab3262ef7e608a88ba604
Charlie Ge <Charlie_ge2023@163.com>
2025-08-01 18:18:40 +0800

    Update 解数独.py
    自己重新写一遍官方题解

--- 解数独.py
+++ 解数独.py
@@ -32,3 +32,40 @@ class Solution:
 
         dfs(0)
 
+
+
+#v2：
+class Solution:       
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        def fill(pos:int):
+            nonlocal valid
+            if pos==len(space):
+                valid=True
+                return
+            i,j=space[pos]
+
+            for digit in range(9):
+                if line[i][digit]==column[j][digit]==table[i//3][j//3][digit]==False:
+                    line[i][digit]=column[j][digit]=table[i//3][j//3][digit]=True
+                    board[i][j]=str(digit+1)
+                    fill(pos+1)
+                    line[i][digit]=column[j][digit]=table[i//3][j//3][digit]=False
+                if valid:
+                    return
+        space=list()
+        column=[[False]*9 for _ in range(9)]
+        line=[[False]*9 for _ in range(9)]
+        table=[[[False]*9 for _ in range(3)] for _ in range(3)]
+        valid=False
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]==".":
+                    space.append([i,j])
+                else:
+                    digit=int(board[i][j])-1
+                    line[i][digit]=column[j][digit]=table[i//3][j//3][digit]=True
+        fill(0)
+        
+
+
