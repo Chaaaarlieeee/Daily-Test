ad8ffccd12eb3684aa52287bafcbecaf3ceac88e
Charlie Ge <Charlie_ge2023@163.com>
2025-08-09 18:50:04 +0800

    Update 搜索旋转排序数组.py
    新的方法降低了耗时，主要是先确定分段点，根据target位于的分段前或后，对排序正常的分段进行二分法

--- 搜索旋转排序数组.py
+++ 搜索旋转排序数组.py
@@ -19,3 +19,37 @@ class Solution:
                     r = mid - 1
         return -1
 
+
+#v2
+class Solution:
+    def findmin(self,nums:list):
+        l=0
+        r=len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]<nums[-1]:
+                r=mid
+            else:
+                l=mid+1
+        return l
+
+
+    def find(self, nums: list, l, r, target):
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+
+
+    def search(self, nums: list[int], target: int) -> int:
+        i = self.findmin(nums)
+        if target > nums[-1]:
+            return self.find(nums, 0, i - 1, target)
+        else:
+            return self.find(nums, i, len(nums) - 1, target)
+
